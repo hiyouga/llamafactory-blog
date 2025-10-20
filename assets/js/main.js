5443424fdddc9db335486e2d5c87fd74b49624cf
inksnow <inksnowhailong@gmail.com>
2025-10-20 23:11:55 +0800

    新建执行文件

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// 全局 JavaScript 代码
+// 这个文件会在每个页面加载时执行
+
+console.log('LlamaFactory Blog 已加载');
+
+const footer = document.getElementsByClassName('footer')[0];
+footer.getElementsByTagName('span').forEach(span => {
+   if(span.innerText.includes('Hugo')) {
+    span.style.display = 'none';
+   }
+
+});
